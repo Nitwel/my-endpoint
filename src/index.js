45d90ede0991b5e3fde195f1edf55417bb5771c6
Nitwel <mail@nitwel.de>
2023-08-17 11:23:04 +0200

    initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+let calls = 0
+
+export default function (router) {
+	router.get('/:region/:city', async (req, res) => {
+
+		const region = req.params.region ?? 'Europe'
+		const city = req.params.city ?? 'Berlin'
+
+		const itemsService = new API.ItemsService('text')
+
+		calls++
+
+		try {
+			const directus = await API.fetch(`http://worldtimeapi.org/api/timezone/${region}/${city}`)
+
+			const data = await directus.json()
+
+			await itemsService.createOne({ text: `This was created at ${data.datetime} relative to ${region}/${city}` })
+
+			res.send(`It is ${data.datetime} and this endpoint has been called ${calls} times`)
+		} catch (error) {
+			res.send(`${error}`)
+		}
+	});
+};
